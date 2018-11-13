0c2588d3d627e03fce4fce17ea4f0f9dad8c1373
xuconngzhe <43348298+xuconngzhe@users.noreply.github.com>
2018-11-13 13:22:58 -0800

    Update acf-merge-tabs.js
    Optimized the code and bug fix

--- acf-merge-tabs.js
+++ acf-merge-tabs.js
@@ -9,7 +9,7 @@
         if($('.acf-postbox .acf-tab-wrap')[0]){
             var parentBox = merge_acf_tabs();
 
-            if($('#wpseo_meta')[0]){
+            if($('#wpseo_meta')[0] && parentBox){
                 merge_yoast_tab('SEO', 'yoast', $('#wpseo_meta'), parentBox['tab'], parentBox['box']); 
             }
         }
@@ -17,35 +17,29 @@
         /* ------------------------------------------- */
 
         function merge_acf_tabs(){
-            var tab_groups = $('.acf-postbox:not(.acf-hidden) > .acf-fields > .acf-tab-wrap');
-            var tab_wraps = $(".acf-postbox:not(.acf-hidden) > .acf-fields > .acf-tab-wrap").parent(".inside");
-            var firstBoxTab = tab_groups.first();
-            var firstBox = tab_wraps.first();
 
-            // Merge the tabs
-            if (tab_groups.length > 1) {
-                firstBoxTab.closest('.acf-fields.inside').siblings('h2.hndle').find('span').html('PAGE CONTENTS AND SETTINGS');
-                firstBoxInstance = acf.getInstance(firstBoxTab);
+            var tabs = acf.getInstances($('.acf-fields.inside >.acf-tab-wrap'));
+            
+            for(var i = 0; i < tabs.length; i++){
+                var tab = tabs[i];
 
-                tab_groups.not(firstBoxTab).find('.acf-tab-group >li').each(function() {
-                    var a = $(this).find('.acf-tab-button');
-                    var field = acf.getInstance($('.acf-field[data-key='+a.data('key')+']'));
-                    firstBoxInstance.addTab(a, field).close();
-                });
-
-                // Merge the tab content
-                if (tab_wraps.length > 1) {      
-
-                    tab_wraps.not(firstBox).each(function() {
-                        $(this).children().appendTo(firstBox);
-                        $(this).parents(".acf-postbox").remove();
-                    });
+                if(i == 0){
+                    tab.$el.closest('.acf-fields.inside').siblings('h2.hndle').find('span').html('PAGE CONTENTS AND SETTINGS');
+                }else{
+                    for( var j = 0; j < tab.tabs.length; j++){
+                        var element = tab.tabs[j].field.$el;
+                        var element_next = element.next();
+                        var element_group_field = element.closest('.acf-postbox');
+                        element.appendTo(tabs[0].tabs[0].field.$el.closest('.acf-fields.inside'));
+                        element_next.appendTo(tabs[0].tabs[0].field.$el.closest('.acf-fields.inside'));
+                        tabs[0].addTab(tab.tabs[j].$el.html(), tab.tabs[j].field).close();
+                        element_group_field.remove();
+                    }
                 }
-                return {'tab': firstBoxInstance, 'box': firstBox};
-            }else if(tab_groups.length == 1){
-                firstBoxTab.closest('.acf-fields.inside').siblings('h2.hndle').find('span').html('PAGE CONTENTS AND SETTINGS');
-                firstBoxInstance = acf.getInstance(firstBoxTab); 
-                return {'tab': firstBoxInstance, 'box': firstBox};
+            }
+
+            if(tabs.length > 0){
+                return {'tab': tabs[0], 'box': tabs[0].tabs[0].field.$el.closest('.acf-fields.inside')};
             }else{
                 return false;
             }
@@ -54,20 +48,20 @@
         /* ------------------------------------------- */
 
         function merge_yoast_tab( component_title, component_slug , component_content_element, parentBoxTab, parentBox ){
+            $(window).on('load', function(){
+                //Yoast SEO Tab
+                var component_content_element_inside = component_content_element.find('>.inside')
+                var a = "<a href='' class='acf-tab-button' data-placement='top' data-endpoint='0' data-key='" + component_slug + "'>" + component_title + "</a>";
+                var field = "<div class='acf-field acf-field-tab' data-key='" + component_slug + "'></div>" + "<div class='acf-field' data-key='" + component_slug + "-content'></div>";
+                var field_instance;
 
-            var component_content_element_inside = component_content_element.find('>.inside')
-
-            //Yoast SEO Tab
-            var a = "<a href='' class='acf-tab-button' data-placement='top' data-endpoint='0' data-key='" + component_slug + "'>" + component_title + "</a>";
-            var field = "<div class='acf-field acf-field-tab' data-key='" + component_slug + "'></div>" + "<div class='acf-field' data-key='" + component_slug + "-content'></div>";
-
-            //Yoast SEO Content
-            parentBox.append(field);
-            $('.acf-field[data-key="' + component_slug + '-content"]').append(component_content_element);
-
-            var field_instance = $('.acf-field-tab[data-key="' + component_slug + '"]');
-            field = new acf.models.TabField(field_instance);
-            parentBoxTab.addTab(a, field).close();
+                parentBox.append(field);
+                field_instance = $('.acf-field-tab[data-key="' + component_slug + '"]');
+                $('.acf-field[data-key="' + component_slug + '-content"]').append(component_content_element_inside);
+                field = new acf.models.TabField(field_instance);
+                parentBoxTab.addTab(a, field).close();
+                component_content_element.remove();
+            });
         }
  
 
